096cd6c0a15eadc02770ff5529910c08646932bd
Amrinder2006 <amrinders.vl.24@nitj.ac.in>
2026-05-01 16:43:54 +0530

    Add files via upload

---
+++ projServer.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public")); // if your HTML is inside public
+
+app.post("/admin-process", (req, res) => {
+    let Email = req.body.Email;
+    let Password = req.body.Password;
+
+    if (Email === "admin@gmail.com" && Password === "123") {
+        res.send("Valid");
+    } else {
+        res.send("Wrong");
+    }
+});
+
+const PORT = process.env.PORT || 2006;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
